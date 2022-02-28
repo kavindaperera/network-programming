f2f49ea3b403a3ad18133c5d173ed0b5142e837e
kavindaperera <perera.kavinda52@gmail.com>
2022-02-28 16:49:24 +0530

    Start `netty` client-server

---
+++ socket-programming/src/main/java/com/nova/netty/models/RequestData.java
@@ -0,0 +1,16 @@
+package com.nova.netty.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class RequestData {
+
+    @Getter
+    @Setter
+    private int intValue;
+
+    @Getter
+    @Setter
+    private String stringValue;
+
+}
---
+++ socket-programming/src/main/java/com/nova/netty/models/ResponseData.java
@@ -0,0 +1,12 @@
+package com.nova.netty.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class ResponseData {
+
+    @Getter
+    @Setter
+    private int intValue;
+
+}
